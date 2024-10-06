97750cae531d120f1a1bae38047bca3a8dddd06a
Carlos Pereira <carlos.dev.pereira@gmail.com>
2024-10-06 23:52:29 +0100

    fix: add missing build files

--- dist/index.js
+++ dist/index.js
@@ -29940,11 +29940,15 @@ Content-Disposition: form-data; name="branch"
 ${branch}
 ---011000010111000001101001`;
         const client = new http_client_1.HttpClient();
-        const res = await client.postJson(url, body, headers);
-        if (!res || !res.result)
+        const rawResponse = await client.post(url, body, headers);
+        if (!rawResponse)
             throw new Error('Missing Cloudflare response body');
-        console.log('Cloudflare deployment successful: ', res.result);
-        return res.result.result;
+        console.log('raw response: ', rawResponse);
+        const responseBody = await rawResponse.readBody();
+        console.log('response body: ', responseBody);
+        const response = JSON.parse(responseBody);
+        console.log('Cloudflare deployment successful: ', response);
+        return response.result.result;
     }
 }
 exports["default"] = Cloudflare;
