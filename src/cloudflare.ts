628cd53395aeac45226264fb7d385ff6d9f7f2f3
Carlos Pereira <carlos.dev.pereira@gmail.com>
2024-10-11 10:20:06 +0100

    fix: implement retries

--- src/cloudflare.ts
+++ src/cloudflare.ts
@@ -26,21 +26,41 @@ export default class Cloudflare {
       '...'
     )
     const url = `https://api.cloudflare.com/client/v4/accounts/${this.accountID}/pages/projects/${projectName}/deployments`
+    const body = `-----011000010111000001101001\r\nContent-Disposition: form-data; name="branch"\r\n\r\n${branch}\r\n-----011000010111000001101001--\r\n\r\n`
     const headers = {
       'Cache-Control': 'no-store, must-revalidate, max-age=0',
       'Content-Type':
         'multipart/form-data; boundary=---011000010111000001101001',
       Authorization: `Bearer ${this.apiToken}`
     }
-    const body = `-----011000010111000001101001\r\nContent-Disposition: form-data; name="branch"\r\n\r\n${branch}\r\n-----011000010111000001101001--\r\n\r\n`
+    console.log('Request body: ', body)
 
-    const client = new HttpClient()
-    const rawResponse = await client.post(url, body, headers)
-    if (!rawResponse) throw new Error('Missing Cloudflare response body')
-    const responseBody = await rawResponse.readBody()
-    const response: CloudflareResponse = JSON.parse(responseBody)
-    console.log('Cloudflare deployment successful: ', response.result)
+    const sendRequest = async () => {
+      const client = new HttpClient()
+      const rawResponse = await client.post(url, body, headers)
+      if (!rawResponse) throw new Error('Missing Cloudflare response body')
+      const responseBody = await rawResponse.readBody()
+      console.log('Response body: ', responseBody, typeof responseBody)
+      const response: CloudflareResponse = JSON.parse(responseBody)
+      console.log('Deployment successful: ', response.result)
 
-    return response.result
+      return response.result
+    }
+
+    let attempt = 0
+    const maxAttempts = 3
+    let result: CloudflareResponse['result'] | undefined
+    do {
+      try {
+        result = await sendRequest()
+      } catch (error) {
+        console.log(`Deploy attempt ${attempt + 1}, failed: `, error)
+        if (attempt >= maxAttempts) throw error
+        await new Promise(resolve => setTimeout(resolve, 2 ** attempt * 1000))
+      }
+      attempt++
+    } while (attempt < maxAttempts && !result)
+
+    return result
   }
 }
